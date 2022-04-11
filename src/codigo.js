d0919e3e305a48b885dc4f43b305120d1c60fe9d
Daddy jose <piopiotrio@gmail.com>
2022-04-11 19:59:58 -0400

    Alfa 2, funciones get implementadas

--- src/codigo.js
+++ src/codigo.js
@@ -1,11 +1,20 @@
 class codigo{
-    constructor(Clave,Prueba){
+    constructor(Clave,Prueba)
+    {
         this.Clave=Clave;
         this.Prueba=Prueba;
     }
     getClave()
     {
         return this.Clave;
+    }
+    setPrueba(intento)
+    {
+        this.Prueba=this.intento;
+    }
+    getPrueba()
+    {
+        return this.Prueba;
     }
 }
 module.exports = codigo;
--- src/torosyvacas.test.js
+++ src/torosyvacas.test.js
@@ -5,4 +5,9 @@ describe("Tests clase toros y vacas", () => {
         const test = new codigo("12345",-1);
       expect(test.getClave()).toEqual("12345");
     });
+    it("Devuelve el intento", (
+        ) => {
+            const test = new codigo("12345",3);
+          expect(test.getPrueba()).toEqual(3);
+        });
   });
