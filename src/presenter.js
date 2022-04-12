0ccbc1b0f73a183069f259fcbb94a53b779d7129
Daddy jose <piopiotrio@gmail.com>
2022-04-11 21:49:13 -0400

    Alfa 10, primera pantalla html, limita los caracteres de 4 6, primer presenter guarda objeto CSG

--- src/presenter.js
+++ src/presenter.js
@@ -1,30 +1,14 @@
+import codigo from './codigo';
+import {torosYvacas,victoria} from './torosyvacas';
 
-import {sumar, multiplicar} from "./operaciones";
-
-const firsts = document.querySelector("#primer-numeros");
-const seconds = document.querySelector("#segundo-numeros");
-const forms = document.querySelector("#sumar-form");
-const divs = document.querySelector("#resultado-divs");
-
-const firstm = document.querySelector("#primer-numerom");
-const secondm = document.querySelector("#segundo-numerom");
-const formm = document.querySelector("#multiplicar-form");
-const divm = document.querySelector("#resultado-divm");
+const CS = document.querySelector("#CodigoSecreto");
+const forms = document.querySelector("#form");
+const div = document.querySelector("#resultado-divs");
 
 forms.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(firsts.value);
-  const secondNumber = Number.parseInt(seconds.value);
-
-  divs.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
-});
-
-formm.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const firstNumber = Number.parseInt(firstm.value);
-  const secondNumber = Number.parseInt(secondm.value);
-
-  divm.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
+  let CSG = new codigo(CS.value);
+  div.innerHTML = "<p>" + CSG.getcodigolength() + "</p>";
+  module.exports={GSG};
 });
